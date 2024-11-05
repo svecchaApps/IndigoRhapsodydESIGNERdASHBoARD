2a2557084bc9db3f9a5a2e73f67d7d96682fe138
Flutterneu <rsmx141@gmail.com>
2024-11-05 11:41:39 +0530

    Baselayout.jsx

--- Indigo Rhapsody Designer Dashboard/src/components/layout/BaseLayout.jsx
+++ Indigo Rhapsody Designer Dashboard/src/components/layout/BaseLayout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Sidebar from "../sidebar/SideBar.jsx";
+import Sidebar from "../sidebar/SideBar";
 import { Outlet } from "react-router-dom";
 import AppBar from "../appbar/AppBar.jsx";
 
