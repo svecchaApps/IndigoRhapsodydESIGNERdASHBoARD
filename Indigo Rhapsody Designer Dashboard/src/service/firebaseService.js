489e6dc4eda9de2be7a5ac88efcc2256fda1ed19
Flutterneu <rsmx141@gmail.com>
2024-11-18 19:43:17 +0530

    .env updated

--- Indigo Rhapsody Designer Dashboard/src/service/firebaseService.js
+++ Indigo Rhapsody Designer Dashboard/src/service/firebaseService.js
@@ -2,14 +2,17 @@
 import { initializeApp } from "firebase/app";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZe8Wl9jPF-IpJQCws9d5DRFM-69ZqDgg",
-  authDomain: "sveccha-11c31.firebaseapp.com",
-  projectId: "sveccha-11c31",
-  storageBucket: "sveccha-11c31.appspot.com",
-  messagingSenderId: "904787268928",
-  appId: "1:904787268928:web:a801ffdbbc6426f72456a5",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export const uploadImageToFirebase = async (file, folder) => {
@@ -36,7 +39,6 @@ export const getFcmToken = async () => {
       vapidKey: "YOUR_VAPID_KEY",
     });
     if (currentToken) {
-
       return currentToken;
     } else {
       console.warn(
